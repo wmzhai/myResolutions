6511578f584ba87d5321fa299adc2bb83b895a6c
Weiming Zhai <wmzhai@gmail.com>
2016-03-26 17:11:10 +0800

    page layout

--- App.jsx
+++ App.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 
-class App extends React.Component {
+export default class App extends React.Component {
   render() {
     return (
       <h1>Hello World</h1>
     )
   }
-}
-
-if(Meteor.isClient) {
-  Meteor.startup(function () {
-    React.render( <App />, document.getElementById("render-target") );
-  });
 }
