0d8ead8376d7df4daa5aa4342ca206d1f5a7d3b6
Weiming Zhai <wmzhai@gmail.com>
2016-03-29 08:30:42 +0800

    insert to database

--- App.jsx
+++ App.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
+Resolutions = new Mongo.Collection("resolutions");
+
 export default class App extends React.Component {
+  addResolution(event) {
+    event.preventDefault();
+    
+    var text = this.refs.resolution.value.trim();
+    Resolutions.insert({
+      text: text,
+      complete: false,
+      createdAt: new Date()
+    });
+
+    this.refs.resolution.value = "";
+  }
   render() {
     return (
-      <h1>Hello World</h1>
+      <div>
+        <h1>My Resolutions</h1>
+        <form className="new-resolution" onSubmit={this.addResolution.bind(this)}>
+          <input
+            type="text"
+            ref="resolution"
+            placeholder="Finish React Meteor Series" />
+        </form>
+      </div>
     )
   }
 }
